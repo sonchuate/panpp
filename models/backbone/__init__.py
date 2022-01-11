49a51eddc16c9ad46aee0cf078f0c67bd3494be7
law930001 <law930001@gmail.com>
2022-01-11 10:24:25 +0000

    efficientNet

--- models/backbone/__init__.py
+++ models/backbone/__init__.py
@@ -1,4 +1,5 @@
 from .builder import build_backbone
 from .resnet import resnet18, resnet50, resnet101
+from .efficientNet import efficentnet_b7
 
-__all__ = ['resnet18', 'resnet50', 'resnet101', 'build_backbone']
+__all__ = ['resnet18', 'resnet50', 'resnet101', 'build_backbone', 'efficentnet_b7']
