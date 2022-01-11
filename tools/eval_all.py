2f74a3266bc109bdd9c1000cfb00749681e53dc5
law930001 <law930001@gmail.com>
2022-01-11 10:23:47 +0000

    eval

---
+++ tools/eval_all.py
@@ -0,0 +1,36 @@
+import os
+import subprocess
+from natsort import natsorted
+
+def eval_tt():
+
+    model_root = '/root/Storage/panpp/checkpoints/pan_pp_r18_tt/'
+    print(model_root)
+    model_list = natsorted(os.listdir(model_root), reverse=True)
+    model_list.remove('checkpoint.pth.tar')
+
+    try:
+        for model in model_list:
+            print(model)
+
+            os.chdir('/root/Storage/panpp')
+
+            subprocess.call([
+                '/root/anaconda3/envs/pan/bin/python', 'test.py', 'config/pan_pp/pan_pp_r18_tt.py',
+                model_root + model
+            ])
+
+            os.chdir('/root/Storage/panpp/eval/tt')
+
+            subprocess.call([
+                '/root/anaconda3/envs/py27/bin/python', 'Deteval.py'
+            ])
+
+            # break
+
+    except:
+        print('error')
+
+
+if __name__ == '__main__':
+    eval_tt()
