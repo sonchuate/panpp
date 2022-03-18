b1da417cb47df94e3aa9fd039bd94b06ab8df9bd
law930001 <law930001@gmail.com>
2022-03-18 06:13:51 +0000

    attn v5

--- models/backbone/__init__.py
+++ models/backbone/__init__.py
@@ -1,5 +1,7 @@
 from .builder import build_backbone
 from .resnet import resnet18, resnet50, resnet101
+from .resnet_csp import resnet18_csp
+
 from .efficientNet import efficentnet_b7
 
-__all__ = ['resnet18', 'resnet50', 'resnet101', 'build_backbone', 'efficentnet_b7']
+__all__ = ['resnet18', 'resnet50', 'resnet101', 'build_backbone', 'efficentnet_b7', 'resnet18_csp']
--- tools/train.sh
+++ tools/train.sh
@@ -1,6 +1,6 @@
 cd ..
 python train.py config/pan_pp/pan_pp_tt.py
-# --resume /root/Storage/panpp/checkpoints/pan_pp_tt/checkpoint.pth.tar
+# python train.py config/pan_pp/pan_pp_tt.py --resume /root/Storage/panpp/checkpoints/pan_pp_tt/checkpoint.pth.tar
 
 # python train.py config/pan_pp/pan_pp_tt_joint_train.py \
 # --resume checkpoints/pan_pp_tt_joint_train/checkpoint.pth.tar
