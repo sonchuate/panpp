7168439cbef0eed716d672ac24e40004e67c0819
law930001 <law930001@gmail.com>
2022-03-18 06:13:06 +0000

    cutmix

--- dataset/pan_pp/pan_pp_tt.py
+++ dataset/pan_pp/pan_pp_tt.py
@@ -387,6 +387,10 @@ class PAN_PP_TT(data.Dataset):
             word_mask[i + 1] = 1
 
         if self.is_transform:
+
+            # if np.random.rand() < 0.5:
+            # img = self.cutmix(img)
+
             img = random_scale(img, self.short_size)
 
         gt_instance = np.zeros(img.shape[0:2], dtype='uint8')
@@ -435,6 +439,8 @@ class PAN_PP_TT(data.Dataset):
                 1], imgs[2], imgs[3], imgs[4], imgs[5:]
             word_mask = update_word_mask(gt_instance, gt_instance_before_crop,
                                          word_mask)
+
+        # cv2.imwrite('1_image.jpg', img)
 
         gt_text = gt_instance.copy()
         gt_text[gt_text > 0] = 1
@@ -495,6 +501,48 @@ class PAN_PP_TT(data.Dataset):
             data.update(dict(gt_words=gt_words, word_masks=word_mask))
 
         return data
+
+    def cutmix(self, img):
+
+        # img_path = self.img_paths[index]
+        # gt_path = self.gt_paths[index]
+
+        # img = get_img(img_path, self.read_type)
+        # bboxes, words = get_ann(img, gt_path)
+
+        # generate mixed sample
+        lam = np.random.beta(0.4, 0.4)
+        rand_index = np.random.randint(0, len(self.img_paths))
+        img_2 = get_img(self.img_paths[rand_index], self.read_type)
+
+        img_2 = cv2.resize(img_2, (img.shape[1], img.shape[0]))
+
+        bbx1, bby1, bbx2, bby2 = self.rand_bbox(img.shape, lam)
+        img[:, bbx1:bbx2, bby1:bby2] = img_2[:, bbx1:bbx2, bby1:bby2]
+        # adjust lambda to exactly match pixel ratio
+        lam = 1 - ((bbx2 - bbx1) * (bby2 - bby1) / (img.size()[-1] * img.size()[-2]))
+
+
+
+        return img
+
+    def rand_bbox(self, size, lam):
+        W = size[1]
+        H = size[0]
+        cut_rat = np.sqrt(1. - lam)
+        cut_w = np.int(W * cut_rat)
+        cut_h = np.int(H * cut_rat)
+
+        # uniform
+        cx = np.random.randint(W)
+        cy = np.random.randint(H)
+
+        bbx1 = np.clip(cx - cut_w // 2, 0, W)
+        bby1 = np.clip(cy - cut_h // 2, 0, H)
+        bbx2 = np.clip(cx + cut_w // 2, 0, W)
+        bby2 = np.clip(cy + cut_h // 2, 0, H)
+
+        return bbx1, bby1, bbx2, bby2
 
     def prepare_test_data(self, index):
         img_path = self.img_paths[index]
